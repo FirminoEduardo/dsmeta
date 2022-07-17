3f7d1449b5de61fb4e89ca55a910b2a855e69bac
FirminoEduardo <dudu.lf@outlook.com>
2022-07-17 17:20:34 -0300

    Axios, useEffect firs request

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -1,8 +1,9 @@
 import NotificationButton from '../NotificationButton';
 import './styles.css';
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import axios from 'axios';
 
 function SalesCard() {
     
@@ -10,6 +11,14 @@ function SalesCard() {
     const max = new Date();
     const [minDate, setMinDate] = useState(new Date(min));
     const [maxDate, setMaxDate] = useState(new Date(max));
+
+    useEffect(() => {
+        axios.get("http://localhost:8080/sales")
+            .then(response => {
+                console.log(response.data);
+            });
+    }, []);
+    
 
     return (
         <div className="dsmeta-card">
